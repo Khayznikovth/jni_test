ce3531dbc717c21984bf91154a40a506ba4a4230
Fedor Khayznikov <khayznikovth@gmail.com>
2016-05-27 15:13:46 +0300

    initial

--- src/main/java/ru/kit/jni/sort/JNISort.java
+++ src/main/java/ru/kit/jni/sort/JNISort.java
@@ -4,5 +4,13 @@ package ru.kit.jni.sort;
  * Created by khayz on 23.05.2016.
  */
 public class JNISort {
+    static {
+        System.loadLibrary("JNI_sort");
+    }
+
     public native int inc(int num);
+
+    public static void main(String[] args) {
+        System.out.println(new JNISort().inc(1));
+    }
 }
